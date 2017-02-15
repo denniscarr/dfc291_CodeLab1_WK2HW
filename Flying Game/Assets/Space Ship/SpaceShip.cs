8acaee06510a8833774f36dbe0bb852746458b70
Unknown <dcarr1@gmail.com>
2017-02-14 21:08:21 -0500

    Tweaked movement & animation

--- Flying Game/Assets/Space Ship/SpaceShip.cs
+++ Flying Game/Assets/Space Ship/SpaceShip.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpaceShip : MonoBehaviour {
 
 	public float velocityDecay = 0.9f;
+	public float topSpeed = 5f;
 
 	Rigidbody rb;
 	Animator animator;
@@ -20,10 +21,12 @@ public class SpaceShip : MonoBehaviour {
 		Vector3 newVelocity = rb.velocity * velocityDecay;
 		newVelocity.x += Input.GetAxis ("Horizontal");
 		newVelocity.y += Input.GetAxis ("Vertical");
+		newVelocity.x = Mathf.Clamp (newVelocity.x, -topSpeed, topSpeed);
+		newVelocity.y = Mathf.Clamp (newVelocity.y, -topSpeed, topSpeed);
 		rb.velocity = newVelocity;
 
 		// Animation
-		animator.SetFloat("x", Input.GetAxis("Horizontal"));
-		animator.SetFloat("y", Input.GetAxis("Vertical"));
+		animator.SetFloat("x", MyMath.Map(rb.velocity.x, -topSpeed, topSpeed, -1f, 1f));
+		animator.SetFloat("y", MyMath.Map(rb.velocity.y, -topSpeed, topSpeed, -1f, 1f));
 	}
 }
