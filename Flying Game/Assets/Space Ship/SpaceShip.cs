3d1320276249d51388894df35baf2a18ffd02cfa
Unknown <dcarr1@gmail.com>
2017-02-14 21:01:47 -0500

    Set up space ship animation

---
+++ Flying Game/Assets/Space Ship/SpaceShip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+
+	public float velocityDecay = 0.9f;
+
+	Rigidbody rb;
+	Animator animator;
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		// Movement
+		Vector3 newVelocity = rb.velocity * velocityDecay;
+		newVelocity.x += Input.GetAxis ("Horizontal");
+		newVelocity.y += Input.GetAxis ("Vertical");
+		rb.velocity = newVelocity;
+
+		// Animation
+		animator.SetFloat("x", Input.GetAxis("Horizontal"));
+		animator.SetFloat("y", Input.GetAxis("Vertical"));
+	}
+}
