7f16269bb71886073dfe80d4fbc9a4ad709bd4e9
Unknown <dcarr1@gmail.com>
2017-02-16 17:00:12 -0500

    i forget what I did, probably something really good though

--- Flying Game/Assets/Enemy/Enemy.cs
+++ Flying Game/Assets/Enemy/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
 	// Health stuff
+	bool dead;
 	public int maxHealth = 10;
 	private int health;
 	public int Health {
@@ -17,7 +18,13 @@ public class Enemy : MonoBehaviour {
 			// See if I died
 			if (health <= 0) {
 				Instantiate (explosion, transform.position, Quaternion.identity);
-				Destroy (gameObject);
+				GetComponent<Rigidbody> ().isKinematic = false;
+				GetComponent<Rigidbody> ().useGravity = true;
+				GetComponent<Rigidbody> ().AddForce (Vector3.up * 25f, ForceMode.Impulse);
+				GetComponent<Rigidbody> ().AddTorque (Random.insideUnitSphere * 50f, ForceMode.Impulse);
+				GetComponent<Animator> ().Stop ();
+				deathParticles.SetActive (true);
+				dead = true;
 			}
 
 			// Increase redness
@@ -32,10 +39,13 @@ public class Enemy : MonoBehaviour {
 
 	// Movement stuff
 	float noiseOffset;
+	public float noiseSpeed = 0.01f;
+	float noiseTime = 0.0f;
 	public float zSpeedMax = 100f;
 	public float zSpeedMin = 5f;
 
 	public GameObject explosion;
+	public GameObject deathParticles;
 
 	DennisGameManager gm;
 	Rigidbody rb;
@@ -50,16 +60,25 @@ public class Enemy : MonoBehaviour {
 	void Update()
 	{
 		// Get zSpeed based on distance from player
-		float zSpeed = MyMath.Map(
-			Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position),
-			0f, 500f, zSpeedMin, zSpeedMax
-		);
+		if (!dead) {
+			float zSpeed = MyMath.Map (
+				              Vector3.Distance (GameObject.FindGameObjectWithTag ("Player").transform.position, transform.position),
+				              0f, 500f, zSpeedMin, zSpeedMax
+			              );
 
-		Vector3 newPos = new Vector3 (
-			MyMath.Map(Mathf.PerlinNoise(Time.time+noiseOffset, 0f), 0f, 1f, -gm.moveRangeX, gm.moveRangeX),
-			MyMath.Map(Mathf.PerlinNoise(0f, Time.time+noiseOffset), 0f, 1f, -gm.moveRangeY, gm.moveRangeY),
-			transform.position.z - zSpeed*Time.deltaTime
-		);
-		rb.MovePosition (newPos);
+			Vector3 newPos = new Vector3 (
+				MyMath.Map (Mathf.PerlinNoise (noiseTime + noiseOffset, 0f), 0f, 1f, -gm.moveRangeX, gm.moveRangeX),
+				MyMath.Map (Mathf.PerlinNoise (0f, noiseTime + noiseOffset), 0f, 1f, -gm.moveRangeY, gm.moveRangeY),
+				transform.position.z - zSpeed * Time.deltaTime
+			);
+
+			rb.MovePosition (newPos);
+
+			noiseTime += noiseSpeed;
+		}
+
+		if (transform.position.z < -50f || transform.position.y < -50f) {
+			Destroy (gameObject);
+		}
 	}
 }
--- Flying Game/Assets/Enemy/EnemySpawner.cs
+++ Flying Game/Assets/Enemy/EnemySpawner.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject enemy;
+
+	public float spawnDelayStart = 5f;
+	public float spawnDelayDecrease = 0.001f;
+	float spawnDelay;
+	float timeSinceLastSpawn;
+
+	DennisGameManager gm;
+
+	void Start() {
+		spawnDelay = spawnDelayStart;
+		timeSinceLastSpawn = spawnDelayStart-1f;
+
+		gm = GameObject.Find ("Dennis Game Manager").GetComponent<DennisGameManager> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+
+		spawnDelay -= spawnDelayDecrease * Time.deltaTime;
+
+		if (timeSinceLastSpawn >= spawnDelay) {
+			SpawnEnemy ();
+			timeSinceLastSpawn = 0.0f;
+		}
+	}
+
+	void SpawnEnemy() {
+		Instantiate (enemy, transform.position, Quaternion.identity);
 	}
 }
